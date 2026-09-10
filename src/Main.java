fb62ddf870f4a86ddfb3f6ab4c750d672c23863b
bertonyyy <fanda.kro@seznam.cz>
2026-09-10 20:44:13 +0200

    Popis vět

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("");
+    System.out.println("O prázdnínách jsem víceméně pracoval na brigádě. Byl jsem na táboře. Chodil jsem na dědinské akce. Vystupoval v kroji. Chodil do hospody :) .");
     }
 
