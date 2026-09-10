2f33334295760c8215b45b7c9bab5a97e85480f7
bertonyyy <fanda.kro@seznam.cz>
2026-09-10 20:47:08 +0200

    Základní údaje

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("O prázdnínách jsem víceméně pracoval na brigádě. Byl jsem na táboře. Chodil jsem na dědinské akce. Vystupoval v kroji. Chodil do hospody :) .");
+
+    System.out.println("František Kročil");
+    System.out.println("Bánov 123, 687 54");
+    System.out.println("17 let");
+    System.out.println("+420 111 222 333");
+    System.out.println("fanda.kro@seznam.cz");
     }
 
